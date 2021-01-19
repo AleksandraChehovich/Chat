8eb043b06e95055e5752db6b88abbbdf6ab405cb
AleksandraChehovich <sanunia22@gmail.com>
2021-01-19 14:25:21 +0300

    refactor: deleted unused state from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,18 @@ import LogIn from './Registration_window';
 const Chat = () => {
     const [messages, setMessages] = useState([]);
     const [name, setName] = useState(localStorage.getItem('chat_name'));
-    // const [isLogIn, setIsLogIn] = useState(localStorage.getItem('chat_name') !== '' ? true : false);
-    const [isLogIn, setIsLogIn] = useState(localStorage.getItem('chat_name') || '');
 
     const addNewMessage = (mesObject) => {
         setMessages(messages.concat([mesObject]));
     };
 
     const changeName = (value) => {
-        setIsLogIn(value);
         setName(value);
     }
 
     return (
         <div className='chat-wrapper'>
-            <LogIn logIn={isLogIn} onSubmit={changeName}/>
+            <LogIn logIn={name} onSubmit={changeName}/>
             <Nav name={name} onClick={changeName}/>
             <MessagesList messages={messages}/>
             <Form name={name} onSubmit={addNewMessage}/>
